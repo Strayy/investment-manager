5dcb4aa5b43f456bae22c9bef6f97244ba624658
Strayyyy <jakobternel@gmail.com>
2024-02-22 12:38:56 +1100

    updated updatePricing endpoint to support non-US exchanges

--- server/routes/stocks.js
+++ server/routes/stocks.js
@@ -91,85 +91,155 @@ router.post("/updatePricing", async (req, res) => {
     try {
         const [exchange, ticker] = req.query.stock.split("_");
 
+        // Supported exchanges
+        const exchangeSettings = {
+            NASDAQ: {
+                suffix: "",
+                exchangeCode: "NMS",
+            },
+            NYSE: {
+                suffix: "",
+                exchangeCode: "NYQ",
+            },
+            ASX: {
+                suffix: ".AX",
+                exchangeCode: "ASX",
+            },
+        };
+
+        // Find the MongoDB model associated with the exchange
         const performanceModel = mongoose.connection.collection(
             `performance-${exchange.toLowerCase()}`,
         );
 
+        let rangeDates = [null, null];
+
+        // Load existing pricing data for requested stock ticker
         const oldPricingData = await performanceModel
             .find({ stock: ticker })
             .sort({ date: -1 })
             .toArray();
 
-        let newPricingData;
-        const failedImport = [];
+        // Returns range to be used in API call for optimisation. E.g., 1d, 1mo, 6mo
+        const rangeToUse = async () => {
+            if (oldPricingData.length === 0) {
+                return "5y";
+            } else {
+                const latestRecordedDate = new Date(oldPricingData[0].date);
+                const currentDate = new Date();
+                const rangeOptions = ["1d", "5d", "1mo", "3mo", "6mo", "1y"];
+                const rangeThresholds = [1, 5, 28, 84, 168, 365];
 
-        let latestDate = null;
+                const daysDifference = (currentDate - latestRecordedDate) / (1000 * 60 * 60 * 24);
+                const rangeIndex = rangeThresholds.findIndex(
+                    (threshold) => daysDifference <= threshold,
+                );
 
-        if (Object.values(oldPricingData).length !== 0) {
-            latestDate = new Date(oldPricingData[0].date);
-            latestDate.setDate(latestDate.getDate() + 1);
-        } else {
-            latestDate = new Date();
-            latestDate.setDate(latestDate.getDate() - 5 * 365);
-        }
+                return rangeOptions[rangeIndex] || "5y";
+            }
+        };
 
-        if (latestDate !== null) {
+        // Ensures that the correct amounts of data are being provided by request. E.g., number of open data points should equal number of close data points
+        const ensureDataCount = (newPricingData) => {
+            const data = newPricingData["chart"]["result"][0];
+
+            const dataCounts = [
+                data["timestamp"].length,
+                data["indicators"]["quote"][0]["open"].length,
+                data["indicators"]["quote"][0]["close"].length,
+                data["indicators"]["quote"][0]["low"].length,
+                data["indicators"]["quote"][0]["high"].length,
+                data["indicators"]["quote"][0]["volume"].length,
+                data["indicators"]["adjclose"][0]["adjclose"].length,
+            ];
+
+            if (!dataCounts.every((element) => element === dataCounts[0])) {
+                throw new Error(
+                    `Data returned from Yahoo finance does not match. Counts: [${dataCounts}] in format [timestamp, open, close, low, high, volume, adjClose]`,
+                );
+            } else {
+                return true;
+            }
+        };
+
+        // Checks to ensure that the data returned from the request is for the same exchange that was requested. Done to avoid issues where a ticker exists on multiple exchanges for different companies. E.g., CAR (Car Group) on ASX vs CAR (Avis) on NASDAQ
+        const ensureExchangeMatch = (newPricingData) => {
+            const exchangeGiven = newPricingData["chart"]["result"][0]["meta"]["exchangeName"];
+
+            if (exchangeSettings[exchange]["exchangeCode"] === exchangeGiven) {
+                return true;
+            } else {
+                throw new Error(
+                    `Data returned from Yahoo finance does not match exchange requested. Exchange requested: ${exchange} which should return ${exchangeSettings[exchange]["exchangeCode"]}. Exchange given from request: ${exchangeGiven}`,
+                );
+            }
+        };
+
+        // Checks to see if requested exchange is in list of supported exchanges
+        if (Object.keys(exchangeSettings).includes(exchange)) {
             const response = await fetch(
-                `https://financialmodelingprep.com/api/v3/historical-price-full/${ticker}?apikey=${
-                    process.env.FMP_KEY
-                }&from=${latestDate.toISOString().split("T")[0]}`,
+                `https://query1.finance.yahoo.com/v8/finance/chart/${ticker}${exchangeSettings[exchange] !== undefined ? exchangeSettings[exchange]["suffix"] : ""}?interval=1d&range=${await rangeToUse()}`,
                 { timeout: 10000 },
             );
 
-            newPricingData = await response.json();
+            const newPricingData = await response.json();
 
-            if (newPricingData.historical !== undefined) {
-                const documents = newPricingData.historical.map((day) => {
+            // Throws an error if an error is returned from the request
+            if (newPricingData["chart"]["result"] === null) {
+                throw new Error(
+                    `Error from Yahoo Finance: ${newPricingData["chart"]["error"]["description"]}`,
+                );
+            }
+
+            // Checks that data counts and exchanges match
+            if (ensureDataCount(newPricingData) && ensureExchangeMatch(newPricingData)) {
+                const pricingData = newPricingData["chart"]["result"][0];
+
+                // Loop through each timestamp in the timestamp array to create an array of objects in the required format for the MongoDB model.
+                const documents = pricingData["timestamp"].map((timestamp, index) => {
                     if (
-                        day["date"] !== null &&
-                        day["open"] !== null &&
-                        day["high"] !== null &&
-                        day["low"] !== null &&
-                        day["close"] !== null &&
-                        day["adjClose"] !== null &&
-                        day["volume"] !== null
+                        oldPricingData.length === 0 ||
+                        (oldPricingData.length > 0 &&
+                            new Date(timestamp * 1000) > new Date(oldPricingData[0].date))
                     ) {
                         return {
                             stock: ticker,
-                            date: new Date(day["date"]),
-                            open: day["open"],
-                            high: day["high"],
-                            low: day["low"],
-                            close: day["close"],
-                            adjClose: day["adjClose"],
-                            volume: day["volume"],
+                            date: new Date(timestamp * 1000),
+                            open: pricingData["indicators"]["quote"][0]["open"][index],
+                            high: pricingData["indicators"]["quote"][0]["high"][index],
+                            low: pricingData["indicators"]["quote"][0]["low"][index],
+                            close: pricingData["indicators"]["quote"][0]["close"][index],
+                            adjClose: pricingData["indicators"]["adjclose"][0]["adjclose"][index],
+                            volume: pricingData["indicators"]["quote"][0]["volume"][index],
                         };
                     } else {
-                        failedImport.push({
-                            message: "Data returned is incomplete",
-                            data: day,
-                        });
                         return;
                     }
                 });
 
-                await performanceModel.insertMany(documents.filter(Boolean));
-            } else {
-                failedImport.push({
-                    data: newPricingData,
-                    message: "Object is empty",
-                });
+                // Only attempt to add to database if documents contains at least one valid entry.
+                if (!documents.every((document) => document === undefined)) {
+                    const filteredDocuments = documents.filter(Boolean);
+
+                    await performanceModel.insertMany(filteredDocuments);
+
+                    rangeDates[0] = new Date(filteredDocuments[0].date);
+                    rangeDates[1] = new Date(filteredDocuments[filteredDocuments.length - 1].date);
+                }
             }
+        } else {
+            throw new Error(
+                `Exchange is not supported. Supported exchanges: ${Object.keys(exchangeSettings)}`,
+            );
         }
 
         res.status(200).json({
-            message: `Updated pricing data for ${exchange}_${ticker} successfully`,
-            from: latestDate.toISOString().split("T")[0],
-            to:
-                Object.values(newPricingData).length !== 0
-                    ? newPricingData.historical[0].date
-                    : new Date().toISOString().split("T")[0],
-            failedImport: failedImport,
+            message:
+                rangeDates[0] !== null && rangeDates[1] !== null
+                    ? `Updated pricing data for ${exchange}_${ticker} successfully`
+                    : `Request successfully sent, but no data added to database.`,
+            from: rangeDates[0],
+            to: rangeDates[1],
         });
     } catch (err) {
         res.status(400).json({ message: err.message });

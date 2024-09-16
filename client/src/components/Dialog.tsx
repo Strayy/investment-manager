dac3b84fff0d86fd57785fc06db3f39e64fd73c8
Strayyyy <jakobternel@gmail.com>
2024-09-16 23:08:34 +1000

    added keyboard shortcut for closing dialog

--- client/src/components/Dialog.tsx
+++ client/src/components/Dialog.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import "../styles/components/_dialog.scss";
 
 function Dialog({
@@ -7,6 +8,21 @@ function Dialog({
     dialogContent: JSX.Element;
     closeAction: (newValue: boolean) => void;
 }): JSX.Element {
+    // Handle keyboard shortcut for closing panel when <esc> key pressed
+    useEffect(() => {
+        const closeWindow = (event: any) => {
+            if (event.key == "Escape") {
+                closeAction(false);
+            }
+        };
+
+        window.addEventListener("keydown", closeWindow);
+
+        return () => {
+            window.removeEventListener("keydown", closeWindow);
+        };
+    });
+
     return (
         <div className='dialog'>
             <div className='dialog-close' onClick={() => closeAction(false)}></div>
